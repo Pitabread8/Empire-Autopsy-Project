bd28f39a714d0d9c612e0c0ae0c3c955236e2528
Sita <sitavemuri8@gmail.com>
2021-10-22 09:35:43 -0700

    Add functionality

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-// function createDiv() {
-//     for (i = 0; i < 24; i++) {
-//         let item = document.createElement('div');
-//         item.className = "grid-item";
-//         item.id = alphabet[i];
-//         grid = document.getElementById('grid');
-//         grid.appendChild(item);
-//     }
-//     divHover();
-// }
-
-function addNone() {
-    let divs = document.getElementsByClassName("grid-item");
-    for (i = 0; i < divs.length; i++) {
-        divs[i].style.boxShadow = "initial";
-    }
+let blurbs = {
+    "head": ["technological advances & scientific discoveries","long blurb about head"],
+    "arm": ["art, architecture & literature","long blurb about arm"],
+    "spine": ["political structure","long blurb about spine"],
+    "heart": ["beliefs systems/religions","long blurb about heart"],
+    "left-hand": ["Major leaders & their accomplishments - Chandraguptya & Chanakya","long blurb about hand1"],
+    "right-hand": ["major leaders & their accomplishments - Asoka","long blurb about hand2"],
+    "ribs": ["social structures","long blurb about ribs"],
+    "stomach": ["human-environment interaction","long blurb about stomach"],
+    "legs": ["labor systems","long blurb about legs"],
+    "feet": ["trade (routes & goods)","long blurb about feet"]
 }
 
 function divHover() {
-    const items = document.getElementsByClassName('grid-item');
+    const items = document.getElementsByClassName('grid-item')
+    let title = document.getElementById('subheading');
+    let text = document.getElementById('text');
     for (i = 0; i < items.length; i++) {
         let div = items[i];
         div.addEventListener('mouseover', () => {
-            // change ink
-            div.style.backgroundColor = "#000";;
+            if (div.classList.length > 1) {
+                title.innerText = `${div.classList[1]}: ${blurbs[div.classList[1]][0]}`;
+                text.innerText = blurbs[div.classList[1]][1];
+            }
         });
     };
-}
-
-let blurbs = {
-    "head": "long blurb about head"
 }
